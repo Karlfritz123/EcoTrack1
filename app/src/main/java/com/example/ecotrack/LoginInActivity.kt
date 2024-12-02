568f2217286a61d77d785c949144f4aef9189676
Karlfritz123 <karlfritz12345678@gmail.com>
2024-12-02 18:05:32 +0800

    update ahy mid

--- app/src/main/java/com/example/ecotrack/LoginInActivity.kt
+++ app/src/main/java/com/example/ecotrack/LoginInActivity.kt
@@ -2,10 +2,13 @@ package com.example.ecotrack
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ecotrack.databinding.ActivityLoginInBinding
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginInActivity : AppCompatActivity() {
     private val binding:ActivityLoginInBinding by lazy {
@@ -13,11 +16,36 @@ class LoginInActivity : AppCompatActivity() {
     }
     private lateinit var auth: FirebaseAuth
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        auth = FirebaseAuth.getInstance()
+        binding.loginButton.setOnClickListener{
+            val userName = binding.userName.text.toString()
+            val password = binding.password.text.toString()
+
+            if(userName.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Please fill all details", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                auth.signInWithEmailAndPassword(userName, password)
+                    .addOnCompleteListener (this){ task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(this, "Sign in successful", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this, Dashboard::class.java))
+                            finish()
+                        }
+                        else{
+                            Toast.makeText(this, "Sign in failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+        }
+
         setContentView(binding.root)
-        binding.button2.setOnClickListener {
+        binding.signUpButton.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
             finish()
         }
--- app/src/main/java/com/example/ecotrack/SignUpActivity.kt
+++ app/src/main/java/com/example/ecotrack/SignUpActivity.kt
@@ -2,23 +2,55 @@ package com.example.ecotrack
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ecotrack.databinding.ActivitySignUpBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 class SignUpActivity : AppCompatActivity() {
     private val binding: ActivitySignUpBinding by lazy{
         ActivitySignUpBinding.inflate(layoutInflater)
     }
+    private lateinit var auth:FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
+        auth = FirebaseAuth.getInstance()
+
+        binding.buttonAlreadyHaveAccount.setOnClickListener{
             startActivity(Intent(this, LoginInActivity::class.java))
             finish()
+        }
+        binding.registerButton.setOnClickListener{
+            val userName = binding.userName.text.toString()
+            val phone = binding.phone.text.toString()
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+
+            if(userName.isEmpty() || phone.isEmpty() || email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Please fill all details", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                auth.createUserWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(this){task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this, LoginInActivity::class.java))
+                            finish()
+                        }
+                        else{
+                            Toast.makeText(this, "Registration Failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
         }
     }
 }
